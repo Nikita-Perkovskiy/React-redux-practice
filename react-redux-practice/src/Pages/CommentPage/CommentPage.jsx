ae22c283def65f6887c36c498fa4fad045d55a61
Nikita Perkovskiy <nikitaperkovskij@gmail.com>
2023-03-15 20:28:42 +0100

    creat message layout + basic version ready

--- react-redux-practice/src/Pages/CommentPage/CommentPage.jsx
+++ react-redux-practice/src/Pages/CommentPage/CommentPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   selectCity,
@@ -6,6 +6,7 @@ import {
   selectLoader,
 } from "../../selects/weather.select";
 import { fetchWeathers } from "../../store/Actions";
+import "./index.scss";
 
 const CommentPage = () => {
   const dispatch = useDispatch();
@@ -17,14 +18,39 @@ const CommentPage = () => {
     dispatch(fetchWeathers(city));
   }, [city]);
 
-  console.log("Forecast", weatherCity);
-  console.log("Forecast", city);
+  // console.log("Forecast", weatherCity);
+  // console.log("Forecast", city);
+
+  const [taskMessage, setTaskMessage] = useState("");
+
+  const getData = async (e) => {
+    e.preventDefault();
+
+    console.log("getData", e);
+  };
 
   return (
     <>
       <div
+        className="d-flex align-items-center"
         style={{ width: "100%", height: "100vh", backgroundColor: "#04260e" }}
-      ></div>
+      >
+        <div className="mx-auto add-task">
+          <form className="form-task" onSubmit={getData}>
+            <input
+              className="task-massage"
+              maxLength={100}
+              placeholder="Description of my new task"
+              type="text"
+              // onChange={updateContent}
+              value={taskMessage}
+            />
+            <button className="task-btn" type="submit">
+              Give a task
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
